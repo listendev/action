71a8811f285357eebf22b880872aea460c61445c
Leonardo Di Donato <leodidonato@gmail.com>
2023-03-17 02:35:19 +0100

    feat(src): execute lstn in the action
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ src/main.ts
@@ -0,0 +1,70 @@
+import * as os from "os";
+import { promises as fs } from "fs";
+import * as path from "path";
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as io from "@actions/io";
+import * as install from "./install";
+
+async function run() {
+    const runnertmp = process.env["RUNNER_TEMP"] || os.tmpdir();
+    const tmpdir = await fs.mkdtemp(path.join(runnertmp, "lstn-"));
+
+    try {
+        const version = core.getInput("lstn");
+        const workdir = core.getInput("workdir");
+        const cwd = path.relative(process.env["GITHUB_WORKSPACE"] || process.cwd(), workdir);
+
+        const lstn = await core.group(
+            "🐬 Installing lstn... https://github.com/listendev/lstn",
+            async() => {
+                return await install.lstn(version, tmpdir);
+            }
+        )
+
+        // TODO: restore cache here
+
+        const exit = await core.group(
+            "🐬 Running lstn...",
+            async(): Promise<number> => {
+
+                process.env["LSTN_GITHUB_API_TOKEN"] = core.getInput("token");
+                return await exec.exec(
+                    lstn,
+                    [
+                        "--help",
+                    ],
+                    {
+                        cwd,
+                    }
+                );
+            }
+        );
+
+        // TODO: save cache here
+
+        if (exit !== 0) {
+            core.setFailed(`status code: ${exit}`)
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error);
+        } else {
+            core.setFailed(`${error}`);
+        }
+    } finally {
+        // Cleanup
+        try {
+            await io.rmRF(tmpdir);
+        } catch (error) {
+            // Suppress these errors
+            if (error instanceof Error) {
+                core.info(`Couldn't clean up: ${error.message}`);
+            } else {
+                core.info(`Couldn't clean up: ${error}`);
+            }
+        }
+    }
+}
+
+run();
