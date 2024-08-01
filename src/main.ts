28a8b39e7ded1be8aa8d63272a955e2ca9477318
Leonardo Di Donato <leodidonato@gmail.com>
2024-08-02 00:56:21 +0700

    fix: stop argus (if active) also in ci-only mode at post step
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -150,7 +150,7 @@ async function run() {
 }
 
 async function post() {
-  const runArgus = core.getInput('ci') == 'true'; // FIXME: switch to core.getBooleanInput() ?
+  const runArgus = core.getInput('ci') == 'true' || core.getInput('ci') == 'only';
   if (runArgus) {
     const isActive = await core.group(
       'Check whether the CI eavesdrop tool is active',
