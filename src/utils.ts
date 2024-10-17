921c35bcc9a9c4f2730eee54565b3277f0f110c7
Leonardo Di Donato <leodidonato@gmail.com>
2024-10-17 18:05:58 +0200

    fix: make checkPath() work also for files that need privileges elevation
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,38 @@
 import {PathLike, promises as fs} from 'fs';
+import * as exec from '@actions/exec';
 
-export async function checkPath(path: PathLike) {
+export async function checkPath(path: PathLike, withSudo = false) {
   try {
-    const stats = await fs.stat(path);
-    if (stats.isFile()) {
-      return {exists: true, isFile: true};
-    } else if (stats.isDirectory()) {
-      return {exists: true, isFile: false};
+    if (withSudo) {
+      let isFile = false;
+      const opts: exec.ExecOptions = {
+        silent: true,
+        listeners: {
+          stdout: (data: Buffer) => {
+            const res = data.toString().trim();
+            isFile = res.startsWith('-') || res.includes('File:');
+          },
+          stderr: () => {
+            throw new Error(
+              `couldn't check "${path.toString()}" path with sudo`
+            );
+          }
+        }
+      };
+
+      await exec.exec('sudo', ['stat', path.toString()], opts);
+
+      return {exists: true, isFile: isFile};
     } else {
-      // Handle other types (unlikely in most cases)
-      return {exists: true, isFile: undefined};
+      const stats = await fs.stat(path);
+      if (stats.isFile()) {
+        return {exists: true, isFile: true};
+      } else if (stats.isDirectory()) {
+        return {exists: true, isFile: false};
+      } else {
+        // Handle other types (unlikely in most cases)
+        return {exists: true, isFile: undefined};
+      }
     }
   } catch (error: any) {
     if (error.code === 'ENOENT') {
