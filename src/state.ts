d8630818e2e4e445f16b76f7b40782de772a286a
Leonardo Di Donato <leodidonato@gmail.com>
2024-11-05 18:07:54 +0100

    feat: create singleton temporary directory provider
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,7 @@
 import * as core from '@actions/core';
+import * as os from 'os';
+import {promises as fs} from 'fs';
+import * as path from 'path';
 
 /**
  * Indicates whether the POST action is running
@@ -9,4 +12,17 @@ export const IsPost = !!core.getState('isPost');
 // This is necessary since we don't have a separate entry point.
 if (!IsPost) {
   core.saveState('isPost', 'true');
+}
+
+export async function tmpdir() {
+  if (!IsPost) {
+    const tmpdir = await fs.mkdtemp(
+      path.join(process.env['RUNNER_TEMP'] || os.tmpdir(), 'lstn-')
+    );
+    core.saveState('LSTN_ACTION_TMPDIR', tmpdir);
+
+    return tmpdir;
+  }
+
+  return core.getState('LSTN_ACTION_TMPDIR');
 }
