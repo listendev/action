0ff95c70f98316775c25178b29e29cd65e9407c6
Leonardo Di Donato <leodidonato@gmail.com>
2024-11-18 15:57:00 +0100

    feat: rename ci input to runtime
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,11 @@ import * as core from '@actions/core';
 /**
  * EavesdropMustRunAlone is true when the eavesdrop tool is the only one that must run.
  */
-export const EavesdropMustRunAlone: boolean = core.getInput('ci') == 'only';
+export const EavesdropMustRunAlone: boolean =
+  core.getInput('runtime') == 'only';
 
 /**
  * EavesdropMustRun is true when the eavesdrop tool will run, either alone or together with other tools.
  */
 export const EavesdropMustRun: boolean =
-  core.getInput('ci') == 'true' || EavesdropMustRunAlone;
+  core.getInput('runtime') == 'true' || EavesdropMustRunAlone;
